15c2a7f42d6681d8416e48d3d96efc7e4e42a895
wendong.hu <wendong.hu@wwwarehouse.com>
2018-07-27 09:20:02 +0800

    study

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Invocation.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Invocation.java
@@ -27,60 +27,60 @@ import java.util.Map;
  */
 public interface Invocation {
 
-    /**
-     * get method name.
-     *
-     * @return method name.
-     * @serial
-     */
-    String getMethodName();
+  /**
+   * get method name.
+   *
+   * @return method name.
+   * @serial
+   */
+  String getMethodName();
 
-    /**
-     * get parameter types.
-     *
-     * @return parameter types.
-     * @serial
-     */
-    Class<?>[] getParameterTypes();
+  /**
+   * get parameter types.
+   *
+   * @return parameter types.
+   * @serial
+   */
+  Class<?>[] getParameterTypes();
 
-    /**
-     * get arguments.
-     *
-     * @return arguments.
-     * @serial
-     */
-    Object[] getArguments();
+  /**
+   * get arguments.
+   *
+   * @return arguments.
+   * @serial
+   */
+  Object[] getArguments();
 
-    /**
-     * get attachments.
-     *
-     * @return attachments.
-     * @serial
-     */
-    Map<String, String> getAttachments();
+  /**
+   * get attachments.
+   *
+   * @return attachments.
+   * @serial
+   */
+  Map<String, String> getAttachments();
 
-    /**
-     * get attachment by key.
-     *
-     * @return attachment value.
-     * @serial
-     */
-    String getAttachment(String key);
+  /**
+   * get attachment by key.
+   *
+   * @return attachment value.
+   * @serial
+   */
+  String getAttachment(String key);
 
-    /**
-     * get attachment by key with default value.
-     *
-     * @return attachment value.
-     * @serial
-     */
-    String getAttachment(String key, String defaultValue);
+  /**
+   * get attachment by key with default value.
+   *
+   * @return attachment value.
+   * @serial
+   */
+  String getAttachment(String key, String defaultValue);
 
-    /**
-     * get the invoker in current context.
-     *
-     * @return invoker.
-     * @transient
-     */
-    Invoker<?> getInvoker();
+  /**
+   * get the invoker in current context.
+   *
+   * @return invoker.
+   * @transient
+   */
+  Invoker<?> getInvoker();
 
 }
